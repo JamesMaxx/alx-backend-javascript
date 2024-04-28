283b4d0d7cfc3fa4fd0f051e46fc3eda5b997664
JamesMaxx <jamesmaxmunene@gmail.com>
2024-04-29 02:30:25 +0300

    13. Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+  const allEmployees = [];
+  for (const dept of Object.values(report.allEmployees)) {
+    for (const employee of dept) {
+      allEmployees.push(employee);
+    }
+  }
+  return allEmployees[Symbol.iterator]();
+}
