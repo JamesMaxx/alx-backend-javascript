c1c8be04b5f8e2c4e1a330f8f338250f20a399a0
JamesMaxx <jamesmaxmunene@gmail.com>
2024-04-29 03:14:36 +0300

    0. Keep every promise you make and only make promises you can keep

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,5 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = "";
+  let result = '';
   for (const employee of reportWithIterator) {
     result += `${employee} | `;
   }
