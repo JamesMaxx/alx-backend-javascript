787f8ddb76c17387d1b107bda463bb447049b88d
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-01 00:04:21 +0300

    6. Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Creates a Set from an array
+ * @param {Array} array - Array of any kind of element
+ * @returns {Set} - A set created from the array
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
