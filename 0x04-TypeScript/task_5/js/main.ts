1313279d9888791c358af30896c343806ae512fc
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-03 23:13:31 +0300

    Add sumMajorCredits and sumMinorCredits functions to main.ts

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,27 @@
+// task_5/js/main.ts
+
+interface MajorCredits {
+  credits: number;
+  brand: string;
+}
+
+interface MinorCredits {
+  credits: number;
+  brand: string;
+}
+
+function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  const totalCredits = subject1.credits + subject2.credits;
+  return { credits: totalCredits, brand: "Major" };
+}
+
+function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  const totalCredits = subject1.credits + subject2.credits;
+  return { credits: totalCredits, brand: "Minor" };
+}
