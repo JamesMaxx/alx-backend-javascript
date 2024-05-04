1bca0fedeaf5fb7c62277e182dcbdc57c2f8fd5d
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-04 17:08:58 +0300

    chore: Refactor promise handling functions to use ES6 syntax

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,23 +1,6 @@
-#!/usr/bin/node
-
-/**
- * Appends handlers to the handleResponseFromAPI function.
- * @param {Promise} promise - The Promise to handle.
- * @returns {Promise} A Promise that resolves to an object or rejects with an Error.
- */
-function handleResponseFromAPI(promise) {
-  // Append a handler for when the Promise resolves
-  const resolveHandler = promise.then(() => {
-    console.log('Got a response from the API'); // Log message on resolution
-    return { status: 200, body: 'success' }; // Return object on resolution
-  });
-
-  // Append a handler for when the Promise rejects
-  const rejectHandler = promise.catch(() => new Error()); // Return empty Error object on rejection
-
-  // Return a Promise that resolves to an object or rejects with an Error
-  return Promise.race([resolveHandler, rejectHandler]);
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-
-// Export the function to make it accessible from other modules
-module.exports = handleResponseFromAPI;
