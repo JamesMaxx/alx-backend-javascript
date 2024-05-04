ae191bc941a28d7a6ba78d57bfc68b4c2475b8a3
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-04 15:11:42 +0300

    Refactor handleResponseFromAPI function to log error and return error object

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,24 @@
+/**
+ * Handle the response from the API
+ * @param {Promise} promise - The promise from the API
+ * @return {Promise} A new promise that resolves with the response object
+ *                   or rejects with an error
+ */
 function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
+      // Got a response from the API
       console.log('Got a response from the API');
+      // Return a success response
       return {
         status: 200,
         body: 'success',
       };
     })
-    .catch(() => {
+    .catch((error) => {
+      // Got a response from the API
       console.log('Got a response from the API');
-      return new Error();
+      // Return an error
+      return error;
     });
 }
