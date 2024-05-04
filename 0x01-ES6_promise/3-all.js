0aa74a2eeec9dbfc438f2d2446f18927576118f8
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-04 17:05:24 +0300

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,19 +1,10 @@
-// 3-all.js
-
 import { uploadPhoto, createUser } from './utils';
 
-/**
- * Handles profile signup by uploading a photo and creating a user.
- */
-export default async function handleProfileSignup() {
-  try {
-    // Use Promise.all to collectively resolve all promises
-    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-
-    // Log the result to the console
-    console.log(` ${photo} ${user.firstName} ${user.lastName}`);
-  } catch (error) {
-    // Log an error message if any of the promises reject
-    console.error('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
