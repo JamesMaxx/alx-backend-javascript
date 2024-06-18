d2a7858fbb8277de20a582dbc032cd779383115e
JamesMaxx <slimjimmymax@outlook.com>
2024-06-18 12:15:19 +0300

    chore: Update stdin.js to handle user input and display a closing message

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,8 @@
-import AppController from '../controllers/AppController';
-import StudentsController from '../controllers/StudentsController';
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-/**
- * Binds the routes to the appropriate handler in the
- * given Express application.
- * @param {Express} app The Express application.
- * @author JamesMaxx <https://github.com/JamesMaxx>
- */
-const mapRoutes = (app) => {
-  app.get('/', AppController.getHomepage);
-  app.get('/students', StudentsController.getAllStudents);
-  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
-};
-
-export default mapRoutes;
-module.exports = mapRoutes;
+process.stdin.on('data', function (data) {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.stdout.write('This important software is now closing\n');
+    process.stdin.end();
+});
