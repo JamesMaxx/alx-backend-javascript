ca0090c8ff11c0924d4b0b808bbf5a17d0057bfc
JamesMaxx <slimjimmymax@outlook.com>
2024-06-21 00:11:32 +0300

    chore: Update author email in package.json

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,11 +3,11 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('floating point whole numbers', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
+    assert.strictEqual(calculateNumber(2.0, 3.0), 5);
   });
 
   it('rounding down b\'s floating point fractional number', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
+    assert.strictEqual(calculateNumber(2.0, 2.4), 4);
   });
 
   it('rounding down a and b\'s floating point fractional number', () => {
